9aa48b4c4436da9d0ed1fe82b31d40a127029d5f
Adam Moskovich <adamam@gmail.com>
2019-12-26 19:43:12 -0700

    Testing: Adds UI and Unit Tests

---
+++ UnitTests Bacc/UnitTests_Bacc.swift
@@ -0,0 +1,33 @@
+//
+//  UnitTests_Bacc.swift
+//  UnitTests Bacc
+//
+//  Created by Adam Moskovich on 12/26/19.
+//  Copyright © 2019 Adam Moskovich. All rights reserved.
+//
+
+import XCTest
+
+class UnitTests_Bacc: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
