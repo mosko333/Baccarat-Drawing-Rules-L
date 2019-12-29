7ed5e553218e0603c427d41bd5d1a9e0d2d417c5
Adam Moskovich <adamam@gmail.com>
2019-12-29 00:21:56 -0700

    CardShoe: Adds ability to recreate a shoe with tests

--- Baccarat Drawing Rules L/Version2/Card Models/CardShoe.swift
+++ Baccarat Drawing Rules L/Version2/Card Models/CardShoe.swift
@@ -12,9 +12,9 @@ class CardShoe {
 
     /// True - unlimited random cards passed back from the shoe
     /// False - shoe will contain a finite number of cards that are shuffled and passed back
-    private let hasUnlimitedCards: Bool
+    private(set) var hasUnlimitedCards: Bool
     /// An array of all the cards in the shoe
-    private(set) var cardsInShoe: [Card] = []
+    private(set) var finiteCardsInShoe: [Card] = []
     // the min number of cards that need to be in the shoe to cantinue dealing
     private let minCardCout = 13
 
@@ -23,12 +23,12 @@ class CardShoe {
         // if cards are unlimited then the shoe will never run out of cards
         guard !hasUnlimitedCards else { return false }
         // otherwise when a min number is reached it will return true
-        return cardsInShoe.count < minCardCout
+        return finiteCardsInShoe.count < minCardCout
     }
 
     // MARK: - init
 
-    init(withUnlimitedCards unlimitedCards: Bool, orNumberOfDecks numberOfDecks: Int = 8) {
+    init(withUnlimitedCards unlimitedCards: Bool = false, numberOfDecks: Int = 8) {
         self.hasUnlimitedCards = unlimitedCards
         // if the shoe is returning unlimited random cards then it doesn't need to create a finite shoe
         if !self.hasUnlimitedCards {
@@ -61,18 +61,20 @@ class CardShoe {
     /// - Shuffles
     /// - Discards the 'burn cards'
     func createNewShoeWith(numberOfDecks: Int) {
+        // a new shoe uses a finite number of cards
+        self.hasUnlimitedCards = false
         // resets the shoe to empty
-        self.cardsInShoe = []
+        self.finiteCardsInShoe = []
 
         // adds all the cards to the shoe
         for suit in CardSuit.allCases {
             for valueName in CardValueName.allCases {
                 let card = (Card(suit: suit, valueName: valueName))
-                cardsInShoe.append(contentsOf: repeatElement(card, count: numberOfDecks))
+                finiteCardsInShoe.append(contentsOf: repeatElement(card, count: numberOfDecks))
             }
         }
 
-        self.cardsInShoe.shuffle()
+        self.finiteCardsInShoe.shuffle()
         // the rules of the game requite a certain number of cards be discarded at the start
         self.burnCards()
     }
@@ -90,7 +92,7 @@ class CardShoe {
     /// Removes and returns the last card in the shoe
     @discardableResult
     private func drawCardFromShoe() -> Card {
-        assert(!cardsInShoe.isEmpty, "This method shouldn't be called on an empty shoe")
-        return self.cardsInShoe.removeLast()
+        assert(!finiteCardsInShoe.isEmpty, "This method shouldn't be called on an empty shoe")
+        return self.finiteCardsInShoe.removeLast()
     }
 }
--- UnitTests Bacc/UnitTest_CardModels.swift
+++ UnitTests Bacc/UnitTest_CardModels.swift
@@ -80,8 +80,56 @@ class UnitTest_CardModels: XCTestCase {
 
     // MARK: - Test cardShoe
 
-    func test_cardShoeInit() {
+    func test_cardShoeInitWithUnlimitedCards() {
+        // test cards in unlimited shoe no decks
+        var cardShoe = CardShoe(withUnlimitedCards: true)
+        XCTAssert(cardShoe.finiteCardsInShoe.isEmpty, "A CardShoe with unlimited cards doesn't need any finite cards")
 
+        // test cards in unlimited shoe with decks
+        cardShoe = CardShoe(withUnlimitedCards: true, numberOfDecks: 4)
+        XCTAssert(cardShoe.finiteCardsInShoe.isEmpty, "A CardShoe with unlimited cards doesn't need any finite cards")
+    }
+
+    func test_cardShoeInitWithFiniteCards() {
+        // test cards in 4 deck finite shoe
+        var cardShoe = CardShoe(withUnlimitedCards: false, numberOfDecks: 4)
+        self.testHelper_cardShoeFiniteCardCount(cardShoe, numOfDecks: 4)
+
+        // test cards in 6 deck finite shoe
+        cardShoe = CardShoe(withUnlimitedCards: false, numberOfDecks: 6)
+        self.testHelper_cardShoeFiniteCardCount(cardShoe, numOfDecks: 6)
+
+        // test cards in 8 deck finite shoe
+        cardShoe = CardShoe(withUnlimitedCards: false, numberOfDecks: 8)
+        self.testHelper_cardShoeFiniteCardCount(cardShoe, numOfDecks: 8)
+    }
+
+    func test_createNewShoe() {
+        // test with initial unlimited shoe
+        var shoe = CardShoe(withUnlimitedCards: true)
+        shoe.createNewShoeWith(numberOfDecks: 7)
+        testHelper_cardShoeFiniteCardCount(shoe, numOfDecks: 7)
+        XCTAssertEqual(shoe.hasUnlimitedCards, false)
+
+        // test with initial finite shoe
+        shoe = CardShoe(withUnlimitedCards: false, numberOfDecks: 9)
+        shoe.createNewShoeWith(numberOfDecks: 6)
+        testHelper_cardShoeFiniteCardCount(shoe, numOfDecks: 6)
+        XCTAssertEqual(shoe.hasUnlimitedCards, false)
+    }
+
+    private func testHelper_cardShoeFiniteCardCount(_ cardShoe: CardShoe, numOfDecks: Int) {
+        // get the current number of finite cards in the shoe
+        let numInShoe = Double(cardShoe.finiteCardsInShoe.count)
+
+        // work out the inital expected count based on 52 cards in a deck
+        var expectedNum = Double(numOfDecks * 52)
+        // cards are burned of the rop of the deck, this can be from 2 - 11 cards
+        // so mid point is 6.5 with an accuracy of 4.5
+        expectedNum -= 6.5
+
+        // test that we are within the expected range
+        XCTAssertEqual(numInShoe, expectedNum, accuracy: 4.5)
     }
 
     func test_cardShoeDrawUnlimited() {
@@ -94,8 +142,19 @@ class UnitTest_CardModels: XCTestCase {
     }
 
     func test_cardShoeDrawFinite() {
-        let cardShoe = CardShoe(withUnlimitedCards: false, orNumberOfDecks: 4)
+        let cardShoe = CardShoe(withUnlimitedCards: false, numberOfDecks: 4)
         // with only 4 decks, the same card should never appear 5 times
         UnitTest_HelperMethods.test_uniqueness(numOfReps: 5, testingMethod: cardShoe.drawCard)
+    }
+
+    func test_isLastHand() {
+        // make a small shoe
+        let shoe = CardShoe(numberOfDecks: 3)
+
+        while shoe.finiteCardsInShoe.count >= 13 {
+            XCTAssertEqual(shoe.isLastHand, false, "It should only be the last hand if less than 13 cards are left")
+            _ = shoe.drawCard()
+        }
+        XCTAssertEqual(shoe.isLastHand, true)
     }
 }
